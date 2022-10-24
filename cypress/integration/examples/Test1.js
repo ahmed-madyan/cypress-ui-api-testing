dead8d10ff8f452749beedab1b5598a5f61ce8de
Ahmed Madyan <ahmed.madyan@vodafone.com>
2022-10-24 13:01:57 +0200

    Update Test1.js

--- cypress/integration/examples/Test1.js
+++ cypress/integration/examples/Test1.js
@@ -4,9 +4,7 @@ describe('Test Suite', function()
     it('Test Case 1', function()
     {
         cy.visit("https://rahulshettyacademy.com/seleniumPractise/#/")
-
     })
-
     // it('Test Case 2', function()
     // {
     //     cy.url.should('contains',"https://rahulshettyacademy.com/seleniumPractise/#/")
