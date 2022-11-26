89dbfcfed1a6643fe7804bcc10780e72cc7fe7a7
Ahmed Madyan <ahmed.madyan@vodafone.com>
2022-11-26 19:41:24 +0200

    UPDATED

--- cypress/integration/examples/Test1.js
+++ cypress/integration/examples/Test1.js
@@ -9,6 +9,6 @@ describe('Test Suite', function()
         cy.visit(url);
         cy.get('.search-keyword').type('tomato')
         cy.wait(2000)
-        cy.get('.product').should('have.length',2)
+        cy.get('.product:visible').should('have.length',2)
         })
 })
