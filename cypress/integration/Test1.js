a29bcc72906babf70218c5ef407fb175b6d0ce32
Ahmed Madyan <ahmed.madyan@vodafone.com>
2022-11-29 16:13:38 +0200

    Update Test1.js

--- cypress/integration/Test1.js
+++ cypress/integration/Test1.js
@@ -1,14 +1,14 @@
 // Mocha test framework standard
 /// <reference types="Cypress" />
 
-describe('Test Suite', function()
+describe('Test Case 1', () =>
 {        
-    it('Test Case 1', function()
+    it('Test Step 1', () =>
     {    
-         const url = "https://rahulshettyacademy.com/seleniumPractise/#/";
+        const url = "https://rahulshettyacademy.com/seleniumPractise/#/";
         cy.visit(url);
         cy.get('.search-keyword').type('tomato')
         cy.wait(2000)
-        cy.get('.product:visible').should('have.length',2)
+        cy.get('.product:visible').should('have.length', 1)
         })
 })
