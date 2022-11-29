eda8b26ff37b23e1e782ed489e98d90117ee2383
Ahmed Madyan <ahmed.madyan@vodafone.com>
2022-11-29 18:07:55 +0200

    UPDATED

--- cypress/integration/TestFramework.cy.js
+++ cypress/integration/TestFramework.cy.js
@@ -5,16 +5,40 @@ describe('Test Case 1', () =>
 {        
     it('Test Step 1', () =>
     {    
-        const url = "https://rahulshettyacademy.com/angularpractice/";
-        cy.visit(url);
-        })
+        const url = "https://rahulshettyacademy.com/angularpractice/"
+        cy.visit(url)
+        cy.get(':nth-child(1) > .form-control').type('Ahmed Madyan')
+        cy.get(':nth-child(2) > .form-control').type('ahmed.madyan01@gmail.com')
+        cy.get('#exampleInputPassword1').type('SDET@v0100')
+        cy.get('#exampleFormControlSelect1').select('Male')
+    })
 })
 
-describe('Test Case 2', () =>
-{        
-    it('Test Step 1', () =>
-    {    
-        const url = "https://rahulshettyacademy.com/angularpractice/";
-        cy.visit(url);
-        })
-})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// describe('Test Case 2', () =>
+// {        
+//     it('Test Step 1', () =>
+//     {    
+//         const url = "https://rahulshettyacademy.com/angularpractice/";
+//         cy.visit(url);
+//         })
+// })
