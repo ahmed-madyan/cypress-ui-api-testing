70f55f93c040e7ea308c1dbf63e424d2ae8873e7
Ahmed Madyan <ahmed.madyan@vodafone.com>
2022-11-29 19:31:48 +0200

    UPDATED

--- cypress/integration/TestFramework.cy.js
+++ cypress/integration/TestFramework.cy.js
@@ -1,16 +1,44 @@
 /// <reference types="Cypress" />
 
+describe('Validate registration form', () =>
+{
+    let data; //closure variable
+    before(() => 
+    {
+    cy.fixture('TestData').then( (TestData) =>
+    {
+        data= TestData
+        cy.visit(data.env.angularpractice_url)
+    })    
+    })        
 
-describe('Test Case 1', () =>
-{        
+    it('Validate input name is required', () =>
+    {    
+        cy.get(':nth-child(1) > .form-control').clear()
+        cy.get(':nth-child(2) > .form-control').click()
+        cy.get(':nth-child(1).form-group > .alert').should('have.text', data.data.Input_Name_Is_Required)
+    })
+
+    it('Validate input name field requires at least 2 characters', () =>
+    {    
+        cy.get(':nth-child(1) > .form-control').type('A').should('have.attr', 'minlength', '2')
+        cy.get(':nth-child(2) > .form-control').click()
+        cy.get(':nth-child(1).form-group > .alert').should('have.text', data.data.Name_At_Least_Two_Characters_Error)
+    })
+
     it('Test Step 1', () =>
     {    
-        const url = "https://rahulshettyacademy.com/angularpractice/"
-        cy.visit(url)
-        cy.get(':nth-child(1) > .form-control').type('Ahmed Madyan')
-        cy.get(':nth-child(2) > .form-control').type('ahmed.madyan01@gmail.com')
-        cy.get('#exampleInputPassword1').type('SDET@v0100')
-        cy.get('#exampleFormControlSelect1').select('Male')
+        cy.get(':nth-child(1) > .form-control').clear()
+        cy.get(':nth-child(1) > .form-control').type(data.data.name)
+        cy.get(':nth-child(4) > .ng-untouched').should('have.value',data.data.name)
+        cy.get(':nth-child(2) > .form-control').type(data.data.email) 
+        cy.get('#exampleInputPassword1').type(data.data.password)
+        cy.get('#exampleFormControlSelect1').select(data.data.gender)
+        cy.get('input.btn').click()
+        cy.get('.alert').should('contain.text', data.data.formSubmittedSuccessMessage)
+        cy.get('.close').should('exist').click()
+        cy.get('.alert').should('not.exist')
+
     })
 })
 
