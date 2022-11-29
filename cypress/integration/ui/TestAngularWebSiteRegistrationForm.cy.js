3c52d64d1a3a56daa479600e52b68927a6d685f1
Ahmed Madyan <ahmed.madyan@vodafone.com>
2022-11-29 22:24:46 +0200

    Update TestAngularWebSiteRegistrationForm.cy.js

--- cypress/integration/ui/TestAngularWebSiteRegistrationForm.cy.js
+++ cypress/integration/ui/TestAngularWebSiteRegistrationForm.cy.js
@@ -12,27 +12,6 @@ describe('Validate registration form', () =>
     })    
     })        
 
-    it('Validate input name is required', () =>
-    {    
-        cy.get(':nth-child(1) > .form-control').clear()
-        cy.get(':nth-child(2) > .form-control').click()
-        cy.get(':nth-child(1).form-group > .alert').should('have.text', data.data.inputNameIsRequiredMessage)
-    })
-
-    it('Validate input name field requires at least 2 characters', () =>
-    {    
-        cy.get(':nth-child(1) > .form-control').type('A').should('have.attr', 'minlength', '2')
-        cy.get(':nth-child(2) > .form-control').click()
-        cy.get(':nth-child(1).form-group > .alert').should('have.text', data.data.nameAtLeastTwoCharactersError)
-    })
-
-    it('Validate input email is required', () =>
-    {    
-        cy.get(':nth-child(2) > .form-control').type('A').clear()
-        cy.get(':nth-child(1) > .form-control').click()
-        cy.get(':nth-child(2) > .alert').should('have.text', data.data.inputEmailIsRequiredMessage)
-    })
-
     it('Validate successfull registration', () =>
     {    
         cy.get(':nth-child(1) > .form-control').clear()
@@ -46,5 +25,36 @@ describe('Validate registration form', () =>
         cy.get('.close').should('exist').click()
         cy.get('.alert').should('not.exist')
 
+    })
+
+    it('Validate input name is required', () =>
+    {    
+        cy.get(':nth-child(1) > .form-control').clear()
+        cy.get(':nth-child(2) > .form-control').click()
+        cy.get(':nth-child(1).form-group > .alert').should('have.text', data.data.inputNameIsRequiredMessage)
+    })
+
+    it('Validate input email is required', () =>
+    {    
+        cy.get(':nth-child(2) > .form-control').type('A').clear()
+        cy.get(':nth-child(1) > .form-control').click()
+        cy.get(':nth-child(2) > .alert').should('have.text', data.data.inputEmailIsRequiredMessage)
+    })
+
+    it('Validate input name field requires at least 2 characters', () =>
+    {    
+        cy.get(':nth-child(1) > .form-control').type('A').should('have.attr', 'minlength', '2')
+        cy.get(':nth-child(2) > .form-control').click()
+        cy.get(':nth-child(1).form-group > .alert').should('have.text', data.data.nameAtLeastTwoCharactersError)
+    })
+
+    it('Validate registration form radio buttons', () =>
+    {    
+        cy.get('#inlineRadio1').should('be.enabled')
+        cy.get(':nth-child(2) > .form-check-label').should('have.text', 'Student')
+        cy.get('#inlineRadio2').should('be.enabled')
+        cy.get(':nth-child(3) > .form-check-label').should('have.text', 'Employed')
+        cy.get('#inlineRadio3').should('be.disabled')
+        cy.get(':nth-child(7) > :nth-child(4) > .form-check-label').should('have.text', 'Entrepreneur (disabled)')
     })
 })
