43873e2fd43c27a6c57cfe481c1e5f6e75e00008
Ahmed Madyan <ahmed.madyan@vodafone.com>
2022-11-29 16:11:09 +0200

    UPDATED

---
+++ cypress/integration/TestFramework.cy.js
@@ -0,0 +1,20 @@
+/// <reference types="Cypress" />
+
+
+describe('Test Case 1', () =>
+{        
+    it('Test Step 1', () =>
+    {    
+        const url = "https://rahulshettyacademy.com/angularpractice/";
+        cy.visit(url);
+        })
+})
+
+describe('Test Case 2', () =>
+{        
+    it('Test Step 1', () =>
+    {    
+        const url = "https://rahulshettyacademy.com/angularpractice/";
+        cy.visit(url);
+        })
+})
