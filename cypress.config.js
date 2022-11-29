d202356fbd6d2e036af5f0bf453b7cf2cc6fe75d
Ahmed Madyan <ahmed.madyan@vodafone.com>
2022-11-30 01:36:09 +0200

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -7,6 +7,6 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    // specPattern: 'cypress/integration/ui/*.js'
+    specPattern: 'cypress/integration/ui/*.js'
   },
 });
