f9000c7514d85821387c66706a37b35a64d0bb5e
Ahmed Madyan <ahmed.madyan@vodafone.com>
2022-11-30 02:01:54 +0200

    Update commands.js

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,25 +1,11 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+Cypress.Commands.add('register', (fName, email, password, gender) =>
+{
+    cy.get(':nth-child(1) > .form-control').clear()
+    cy.get(':nth-child(1) > .form-control').type(fName)
+    cy.get(':nth-child(4) > .ng-untouched').should('have.value', fName)
+    cy.get(':nth-child(2) > .form-control').clear()
+    cy.get(':nth-child(2) > .form-control').type(email) 
+    cy.get('#exampleInputPassword1').type(password)
+    cy.get('#exampleFormControlSelect1').select(gender)
+    cy.get('input.btn').click()
+})
